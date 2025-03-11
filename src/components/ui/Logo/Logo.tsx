8f46a4cb09c1f22d6cb53bf004c7cdca2fa0daf4
Braandex <Aadewuyi904@gmail.com>
2025-03-11 11:15:03 +0100

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,7 +1,5 @@
 import cx from 'classnames'
-
 import { Href } from 'components/navigation'
-
 
 type LogoProps = {
   className?: string
@@ -13,22 +11,22 @@ const Logo: React.FC<LogoProps> = (props) => {
   return (
     <Href to="/" className={cx('flex items-center', className)}>
       <svg
-  width="71"
-  height="16"
-  viewBox="0 0 71 16"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <text
-    x="0"
-    y="12"
-    fill="white"
-    font-family="sans-serif"
-    font-size="12"
-    font-weight="bold"
-  >
-    Vookie
-  </text>
-</svg>
+        width="71"
+        height="16"
+        viewBox="0 0 71 16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text
+          x="0"
+          y="12"
+          fill="white"
+          font-family="sans-serif"
+          font-size="12"
+          font-weight="bold"
+        >
+          Vookie
+        </text>
+      </svg>
     </Href>
   )
 }
