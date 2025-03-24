546740a2603f7410020ba4479be6293740842a81
Braandex <Aadewuyi904@gmail.com>
2025-03-24 17:42:11 +0100

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -2,7 +2,6 @@ import cx from 'classnames'
 import { Href } from 'components/navigation'
 import 'src/components/ui/styles.css'
 
-
 type LogoProps = {
   className?: string;
 };
@@ -12,7 +11,7 @@ const Logo: React.FC<LogoProps> = (props) => {
 
   return (
     <Href to="/" className={cx('flex items-center justify-center', className)}>
-      <span className="font-rookie font-bold text-white text-vookie">
+      <span className="font-rookie font-bold text-white text-vookie spaced-text">
         VOOKIE
       </span>
     </Href>
